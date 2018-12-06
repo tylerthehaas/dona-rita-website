782bbd98bf06e9bcfbda5c672274feb7cadfb0e7
Tyler Haas <tyler.haas@.octanner.com>
2018-12-05 21:59:59 -0700

    allow for no picture to be rendered

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -7,8 +7,8 @@ export default function Recipe({ img, title, children }) {
 		<div className="grid">
 			<div className="col fluid md-6 md-push-1 lg-6 lg-push-2">
 				<h1>{title}</h1>
+				{img && <FakeImg className="-three-x-two" img={img} />}
 				<span className="measure">{children}</span>
-				<FakeImg className="-three-x-two" img={img} />
 			</div>
 		</div>
 	);
