954c521bcb505bb07b71796ae3fe1ee0fc2bcfc7
Tyler Haas <haasdevelopment@gmail.com>
2019-04-04 13:04:03 -0600

    add rest of reviews

--- src/pages/buy/index.js
+++ src/pages/buy/index.js
@@ -82,7 +82,8 @@ function ShopsPage({ data }) {
               <a href="mailto:carolinapacific@protonmail.com">
                 carolinapacific@protonmail.com
               </a>
-              . Distributors welcome.
+              .<br />
+              Distributors welcome.
             </p>
           </div>
         </div>
@@ -94,7 +95,97 @@ function ShopsPage({ data }) {
         </div>
       </Section>
 
-      <Contact />
+      <Section>
+        <div className="grid">
+          <h2 className="h1 col" style={{ textAlign: "center", width: "100%" }}>
+            Reviews
+          </h2>
+        </div>
+        <div className="grid">
+          <div className="col md-12 lg-12">
+            <blockquote>
+              I was having some friends over for dinner, which usually gets
+              complicated because I have friends who are meat-eaters,
+              vegetarians, vegans, and pescatarians. It’s a lot of work to cook
+              separate foods for all these special diets, but then, I came
+              across Caybana Moon Jerk Sauce and Marinade. I followed some of
+              their recipes on their Web site, and my dinner party was so much
+              easier than in the past. I simply got out a bunch of zip-top bags,
+              and put either the mild or spicy Caybana Moon Jerk Sauce and
+              Marinade into each bag. Then, I put chicken pieces in one bag,
+              tofu and jackfruit in another, and shrimp, scallops and fish in
+              the third bag. After marinating them for a little while, I baked
+              them in the oven. I served a big tossed salad, which everyone
+              could eat, and served a fresh fruit platter for dessert. Everybody
+              raved about the Caybana Moon Jerk Sauce and Marinade, whether it
+              was the mild or spicy, and for the first time, cooking for all
+              these friends with different diets was easy!
+              <span> &#8212; Diane A., Malibu, CA</span>
+            </blockquote>
+          </div>
+          <div className="col md-12 lg-12">
+            <blockquote>
+              There is nothing better than a Caybana Moon Spicy Jerk Sauce and
+              Marinade Grilled Shrimp Po' Boy. So, easy!!! Simply simmer some
+              shelled, fresh shrimp in your sauce until done, and slice a nice
+              fresh French bread roll almost in half, lengthwise. Put your spicy
+              jerk shrimp on the roll along with some shredded lettuce, tomato
+              slices, and Caybana Moon Spicy Jerk Sauce and Marinade mixed with
+              a little mayonnaise. For my fried Po’Boy, I marinate about 1 1/2
+              pounds shelled, fresh shrimp in some Caybana Moon Spicy Jerk Sauce
+              and Marinade for about 15 minutes. Dip marinated shrimp into about
+              a cup of buttermilk, and dredge through a coating mix of about 1
+              1/2 cups all-purpose flour and 1 cup cornmeal with a little salt
+              sprinkled into the mix. Fry over medium high heat in a high smoke
+              point oil, like coconut oil or avocado oil. Layer the fried shrimp
+              onto your sliced French bread roll, with shredded lettuce, juicy
+              tomato slices, mild onion slices, and a half and half mixture of
+              Caybana Moon Spicy Jerk Sauce and Marinade and mayonnaise. Sorry
+              for the long review, but I just couldn’t help myself. Both these
+              Po’ Boys are so yummy!!!
+              <span> &#8212; Nora B., New Orleans, Louisiana</span>
+            </blockquote>
+          </div>
+          <div className="col md-12 lg-12">
+            <blockquote>
+              I marinated some sirloin steaks in your Caybana Moon Mild Jerk
+              Sauce and Marinade for about 4 hours, and then cooked them on the
+              grill. I served them with corn-on-the-cobb, and a big baked potato
+              with butter, sour cream, shredded cheese and bacon. I served some
+              fresh Caybana Moon Mild Jerk Sauce and Marinade on the side as a
+              steak dipping sauce, and this added such a nice flavor to my
+              steaks! The flavorful Caribbean spices from your sauce really made
+              our steaks super tasty, and very moist and tender! Definitely
+              going to make this meal again. It was a big hit!!!
+              <span> &#8212; Kirk D., Des Moines, IA</span>
+            </blockquote>
+          </div>
+          <div className="col md-12 lg-12">
+            <blockquote>
+              I took some live Maryland Blue Crabs, put them in a big pot with a
+              good quality beer in the bottom, and layered the crabs with some
+              of your Caybana Moon Spicy Jerk Sauce and Marinade. I steamed them
+              for around 45 minutes. I served them with corn-on-the-cobb and
+              potato salad, and I served fresh Caybana Moon Spicy Jerk Sauce and
+              Marinade on the side as a dipping sauce! Everyone liked this spicy
+              Caribbean alternative to the usual Chesapeake Bay seasoning!
+              <span> &#8212; Keith W., Salisbury, MD</span>
+            </blockquote>
+          </div>
+          <div className="col md-12 lg-12">
+            <blockquote>
+              I mixed some Caybana Moon Spicy Jerk Sauce and Marinade with equal
+              parts mayonnaise, and dipped my fried fish and hush puppies into
+              it. Very tasty!
+              <span> &#8212; Wyatt J., Rock Hill, SC</span>
+            </blockquote>
+          </div>
+        </div>
+      </Section>
+
+      <Section className="-white">
+        <Contact />
+      </Section>
     </div>
   );
 }
