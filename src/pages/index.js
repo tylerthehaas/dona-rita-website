eccce9efd15395ad1ff5de2e3d19f44b7076d1f9
Tyler Haas <haasdevelopment@gmail.com>
2019-04-04 12:42:42 -0600

    switch back to image on home

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import sauceMild from "../images/mildhomepg1.jpg";
 import sauceSpicy from "../images/spicyhomepg1.jpg";
 import jerkCookout from "../images/Jerk_chicken_july_05.jpg";
 import homeBanner from "../images/banner.jpg";
+import jerkPork from "../images/bbqjerkchicgrill.jpg";
 
 function handleScriptLoad() {
   if (typeof window !== `undefined` && window.netlifyIdentity) {
@@ -123,26 +124,8 @@ function IndexPage({ data, location }) {
         </div>
 
         <div className="grid">
-          <div className="col fluid md-10 md-push-1 lg-10 lg-push-1">
-            <blockquote>
-              I was having some friends over for dinner, which usually gets
-              complicated because I have friends who are meat-eaters,
-              vegetarians, vegans, and pescatarians. It’s a lot of work to cook
-              separate foods for all these special diets, but then, I came
-              across Caybana Moon Jerk Sauce and Marinade. I followed some of
-              their recipes on their Web site, carolinapacificfoods.com, and my
-              dinner party was so much easier than in the past. I simply got out
-              a bunch of zip-top bags, and put either the mild or spicy Caybana
-              Moon Jerk Sauce and Marinade into each bag. Then, I put chicken
-              pieces in one bag, tofu and jackfruit in another, and shrimp,
-              scallops and fish in the third bag. After marinating them for a
-              little while, I baked them in the oven. I served a big tossed
-              salad, which everyone could eat, and served a fresh fruit platter
-              for dessert. Everybody raved about the Caybana Moon Jerk Sauce and
-              Marinade, whether it was the mild or spicy, and for the first
-              time, cooking for all these friends with different diets was easy!
-              <span> &#8212; Diane A, Malibu, CA</span>
-            </blockquote>
+          <div className="col fluid md-7 md-push-1 lg-6 lg-push-2">
+            <FakeImg className="-two-x-three" img={jerkPork} />
           </div>
         </div>
       </Section>
